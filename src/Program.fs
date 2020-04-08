e0c7046a4c55c42ef9dc0a86ae88f2071e0f68d3
Wouter Breukink <wouter@breukink.co.uk>
2020-04-08 20:04:37 +0100

    Bootstrapping first version of onedrive-cli

---
+++ src/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open Microsoft.Identity.Client
+open Microsoft.Graph
+open Microsoft.Graph.Auth
+open FSharp.Control.Tasks.V2.ContextInsensitive
+
+[<EntryPoint>]
+let main argv =
+
+    let clientId = "52b727d4-693f-4a5c-b420-5e9d0afaf8c6"
+    let tokenStorage = "user.token"
+
+    // Build a client application.
+    let application = 
+        PublicClientApplicationBuilder
+            .Create(clientId)
+            .Build();
+
+    TokenSaver.register application.UserTokenCache tokenStorage
+
+    let someCallback (dcr : DeviceCodeResult) = 
+        printfn "%s" dcr.Message
+        Threading.Tasks.Task.CompletedTask
+
+    // Create an authentication provider by passing in a client application and graph scopes.
+    let authProvider = new DeviceCodeProvider(application, ["files.read.all"], (fun dcr -> someCallback dcr));
+    // Create a new instance of GraphServiceClient with the authentication provider.
+    let client = new GraphServiceClient(authProvider);
+
+    let drive = task {
+        let! drive = client.Me.Drive.Request().GetAsync()
+
+        let! drives = client.Drives.Request().GetAsync()
+
+        ()
+    } 
+    
+    drive.Result
+
+    0
---
+++ src/TokenSaver.fs
@@ -0,0 +1,26 @@
+﻿module TokenSaver
+
+open Microsoft.Identity.Client
+open System.IO
+
+let register (tokenCache : ITokenCache) cacheFile =
+
+    let locker = obj ()
+
+    let asDelegate f =
+        new TokenCacheCallback (f)
+
+    let beforeAccess (args : TokenCacheNotificationArgs) = 
+        lock locker (fun () ->
+            if File.Exists cacheFile then
+                args.TokenCache.DeserializeMsalV3(File.ReadAllBytes(cacheFile))
+        )
+
+    let afterAccess (args : TokenCacheNotificationArgs) =
+        lock locker (fun () ->
+            if args.HasStateChanged then
+                File.WriteAllBytes(cacheFile,args.TokenCache.SerializeMsalV3())
+        )
+        
+    tokenCache.SetBeforeAccess (beforeAccess |> asDelegate )
+    tokenCache.SetAfterAccess (afterAccess |> asDelegate)
